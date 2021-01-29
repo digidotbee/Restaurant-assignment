4353fe2fe5b1c1e7890221610d8959afb879bb5f
Jon-Nutman <jonnut730@gmail.com>
2021-01-29 17:31:56 -0500

    menu function

---
+++ menu-function.js
@@ -0,0 +1,64 @@
+const appQuery = document.querySelector("#apps-menu")
+const entQuery = document.querySelector("#entrees-menu")
+const desQuery = document.querySelector("#desserts-menu")
+fetch('https://obscure-tundra-54269.herokuapp.com/casual-dining')
+.then((resp) => resp.json())
+.then((data) => {
+    console.log(data)
+// App Menu Function
+    const appMenu = data.appetizers.map(function (item) {
+        console.log(item.name)
+       return `     
+                    <h2>${item.name}</h2>
+                    <p>${item.description}</p>
+                    <p>$${item.price}</p>`
+
+    
+    })
+// Entree Menu Function
+    const entMenu = data.entrees.map(function (item) {
+    console.log(item.name)
+   return `     
+                <h2>${item.name}</h2>
+                <p>${item.description}</p>
+                <p>$${item.price}</p>`
+                
+
+    })
+// Dessert Menu Function
+    const desMenu = data.entrees.map(function (item) {
+        console.log(item.name)
+       return `     
+                    <h2>${item.name}</h2>
+                    <p>${item.description}</p>
+                    <p>$${item.price}</p>`
+                    
+    
+     })
+
+// These insert the stuffs into the html
+
+    const menuString = appMenu.join('')
+            appQuery.innerHTML = appMenu
+
+    const entString = entMenu.join('')
+    entQuery.innerHTML = entMenu
+
+    const desString = desMenu.join('')
+            desQuery.innerHTML = desMenu
+})
+
+// const entQuery = document.querySelector("#entrees-menu")
+// fetch('https://obscure-tundra-54269.herokuapp.com/casual-dining')
+// .then((resp) => resp.json())
+// .then((data) => {
+// const entMenu = data.entrees.map(function (item) {
+//     console.log(item.name)
+//    return `     
+//                 <h2>${item.name}</h2>
+//                 <p>${item.description}</p>
+//                 <p>$${item.price}</p>`
+// })
+// const entString = entMenu.join('')
+// entQuery.innerHTML = entMenu
+// })
