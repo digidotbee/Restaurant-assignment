ea28c34c7e79df461bccb7200c22348ed4842ac4
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-01-31 09:45:10 -0500

    some more javaScript tinkering

--- story.js
+++ story.js
@@ -1,16 +1,17 @@
-const storyTime = `<p>Stepping up to order at Uncle John's Smokehouse Foodtruck is like
+const storyTime = `<p> Stepping up to order at Uncle John's Smokehouse Foodtruck is like
 stepping into the technicolor mind of Uncle John himself.  The Northern Delaware 
 native who has infused his entire being 
 into the craft of whole-hog barbecue. 
 The old ice cream truck turned barbecue heaven, open since August 2015, 
-features the rustic hand-painted signs of roasted meats offered and chalkboard menu items is 
+featuring the rustic hand-painted sign of meats offered and chalkboard menu is 
 the perfect backdrop for Uncle John's smoky delights. Son of a 
 pig farmer and pitmaster of the South Carolina barbecue tradition, 
 Uncle John incorporates regional delights into the food truck's repertoire including 
-Scrapple hash and chicken bog in addition to his Eastern North Delaware 
-style whole hog barbecue with its vinegary red-pepper flecked mop, 
-while his farm fresh seasonal sides utilize the properties of wood smoke, fire, 
-and hog fat as law. Pair that with a killer Scotch menu and the musical stylings of friends and musicians and you’ve got a food destination of the nth degree.</p>`
+Scrapple hash and chicken bog in addition to his Northern Delaware 
+style whole hog barbecue with its vinegary red-pepper flecked mop.  Meanwhile, his farm fresh 
+seasonal sides utilize the properties of wood smoke, fire, 
+and hog fat as a law. Pair that with a killer Scotch menu and the musical stylings 
+of friends and musicians and you’ve got a food destination of the nth degree.</p>`
 
  
 document.querySelector(".story-content").innerHTML = storyTime
